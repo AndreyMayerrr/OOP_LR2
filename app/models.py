8b18af3cf0990066efde60e7e310c820fcb43cee
AndreyMayerrr <163112886+AndreyMayerrr@users.noreply.github.com>
2025-05-22 14:06:23 +0600

    Лабораторная 3

--- app/models.py
+++ app/models.py
@@ -1,22 +1,26 @@
-# models.py
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 from .database import Base
 
-class Todo(Base):
+class TodoList(Base):
     __tablename__ = "todos"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True)
+    name = Column(String, index=True)
+    completed_count = Column(Integer, nullable=False, default=0)  # Количество выполненных задач
+    total_count = Column(Integer, nullable=False, default=0)      # Общее количество задач
+    deleted_at = Column(DateTime, nullable=True)                  # Дата удаления (soft delete)
 
-    items = relationship("Item", backref="todo", cascade="all,delete")
+    items = relationship("Item", backref="todo", cascade="all,delete")  # Связь с элементами
+
 
 class Item(Base):
     __tablename__ = "items"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
+    name = Column(String)
     text = Column(String)
     is_done = Column(Boolean, default=False)
-
-    todo_id = Column(Integer, ForeignKey("todos.id"))
+    todo_id = Column(Integer, ForeignKey("todos.id"))              # Связь с ToDoList
+    deleted_at = Column(DateTime, nullable=True)                  # Дата удаления (soft delete)

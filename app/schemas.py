3f9f0611f369f59a561df560ff93dec237472f02
AndreyMayerrr <163112886+AndreyMayerrr@users.noreply.github.com>
2025-05-22 14:06:35 +0600

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,44 +1,31 @@
-# schemas.py
 from pydantic import BaseModel
 from typing import Optional, List
 
 class TodoBase(BaseModel):
-
     name: str
 
 class TodoCreate(TodoBase):
-
     pass
 
 class TodoUpdate(TodoBase):
-
-    name: Optional[str] = None  # добавляем возможность частичного обновления
+    name: Optional[str] = None
 
 class Todo(TodoBase):
-
     id: int
+    progress: float  # Процент выполнения задач
 
     class Config:
-        from_attributes = True  # Позволяет преобразовывать объекты SQLAlchemy в модели Pydantic
+        from_attributes = True
 
 class ItemBase(BaseModel):
-
     name: str
     text: str
     is_done: bool = False
 
 class ItemCreate(ItemBase):
-
     pass
 
-class ItemUpdate(ItemBase):
-
-    name: Optional[str] = None
-    text: Optional[str] = None
-    is_done: Optional[bool] = None
-
 class Item(ItemBase):
-
     id: int
     todo_id: int
 
